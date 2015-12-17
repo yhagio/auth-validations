239f56f2e00b53b16210e4fd210c092a4b644c85
Yuichi Hagio <yhagio87@gmail.com>
2015-12-16 21:48:22 -0500

    Updated API: New method: validatePassword New method: validateStringLength0to50

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 ### Auth-Validations
 This is a npm module that validates common inputs for
 user registration process.
+
+## Install
+```
+npm install auth-validations
+```
+In your javascript file,
+```
+var av = require('av');
+```
+---
+
+## API
+
+### validateEmail(email)
+Return error message (String). 
+If the error message length is 0, the validation passed successfully.
+
+### validatePassword(password)
+Returns an array of error messages (String).
+If the array is empty, the validation passed successfully.
+
+### validateStringLength0to50(text)
+Return error message (String).
+If the error message length is 0, the validation passed successfully.
--- src/index.js
+++ src/index.js
@@ -1,12 +1,70 @@
 module.exports = {
-
+  /**
+   * Validate user's email format
+   *
+   **/
   validateEmail: function(email){
+    var errorMessage = '';
     var regex = /\S+@\S+\.\S+/;
     var eamil = email.trim();
-    if(!regex.test(email) || email.length === 0) {
-      return false;
-    } else {
-      return true;
+    if (!regex.test(email) || email.length === 0) {
+      errorMessage = '* Email must be in valid format';
+    } 
+
+    if (email.length > 150) {
+      errorMessages = '* Email is too long, please use shorter email address';
     }
+
+    return errorMessage;
+  },
+
+  /**
+   * Validate user's password format
+   *
+   **/
+  validatePassword: function(password){
+    var errorMessages = [];
+
+    if (password.length > 50) {
+      errorMessages.push('* Must be fewer than 50 chars');
+    }
+
+    if (password.length < 8) {
+      errorMessages.push('* Must be longer than 7 chars');
+    }
+
+    if (!password.match(/[\!\@\#\$\%\^\&\*]/g)) {
+      errorMessages.push('* Missing a symbol(! @ # $ % ^ & *)');
+    }
+
+    if (!password.match(/\d/g)) {
+      errorMessages.push('* Missing a number');
+    }
+
+    if (!password.match(/[a-z]/g)) {
+      errorMessages.push('* Missing a lowercase letter');
+    }
+
+    if (!password.match(/[A-Z]/g)) {
+      errorMessages.push('* Missing an uppercase letter');
+    }
+
+    return errorMessages;
+  },
+
+  /**
+   * Validate user's text input length
+   * Must be 1 - 50 characters
+   **/
+  validateStringLength0to50: function(text){
+    var errorMessage = '';
+    if (text.trim().length > 50) {
+      errorMessage = 'Cannot be more than 50 characters';
+    } else if (text.trim().length <= 0) {
+      errorMessage = 'Cannot be empty';
+    } else {
+      errorMessage = '';
+    }
+    return errorMessage;
   }
 };
